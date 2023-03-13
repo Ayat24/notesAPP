345b2d7a503106d48504b0d0915803e43f14434c
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-13 19:26:03 +0200

    create custom icon

--- lib/views/widget/custom_app_bar.dart
+++ lib/views/widget/custom_app_bar.dart
@@ -20,21 +20,3 @@ class CustomAppBar extends StatelessWidget {
   }
 }
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-      height: 45,
-    width: 45,
-    decoration: BoxDecoration(
-      color: Colors.white
-    ),
-    child: Center(child: Icon(
-      Icons.search,
-    ),
-    ),
-    );
-  }
-}
