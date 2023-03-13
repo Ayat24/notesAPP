912aa6614abe55961dce48bb9820f6a7371bdc51
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-13 20:26:41 +0200

    add expanded

--- lib/views/widget/notes_view_body.dart
+++ lib/views/widget/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
+import '';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -18,8 +20,20 @@ class NotesViewBody extends StatelessWidget {
             title: 'Notes',
             icon: Icons.search,
           ),
+          Expanded(
+            child: NotesListView(),
+          ),
         ],
       ),
     );
+  }
+}
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
