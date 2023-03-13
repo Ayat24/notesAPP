6ddd7f855c53ce87282bb03c669c50ea34eeb176
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-13 19:12:50 +0200

    add text

--- lib/views/widget/custom_app_bar.dart
+++ lib/views/widget/custom_app_bar.dart
@@ -5,6 +5,15 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 28,
+          ),
+        ),
+      ],
+    );
   }
 }
