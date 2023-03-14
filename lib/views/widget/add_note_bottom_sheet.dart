179cc461e622cf7224b93e6e72fde63f1440dd63
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-14 20:47:49 +0200

    add custom text field to add note bottom sheet

--- lib/views/widget/add_note_bottom_sheet.dart
+++ lib/views/widget/add_note_bottom_sheet.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'custom_text_field.dart';
 
 class AddNoteBottomsheet extends StatelessWidget {
   const AddNoteBottomsheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        CustomTextField(),
+      ],
+    );
   }
 }
