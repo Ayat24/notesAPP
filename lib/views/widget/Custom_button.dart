72b73a8259bffc490c34c9533934788706eb0a14
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-14 22:28:29 +0200

    add width media query

--- lib/views/widget/Custom_button.dart
+++ lib/views/widget/Custom_button.dart
@@ -5,6 +5,8 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: MediaQuery.of(context).size.width,
+    );
   }
 }
