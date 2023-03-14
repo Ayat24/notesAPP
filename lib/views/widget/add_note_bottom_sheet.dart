31bb18fef60c14356f1d66f8262762a178d80b18
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-14 23:22:00 +0200

    add container

--- lib/views/widget/add_note_bottom_sheet.dart
+++ lib/views/widget/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_text_field.dart';
+import 'Custom_button.dart';
 
 class AddNoteBottomsheet extends StatelessWidget {
   const AddNoteBottomsheet({super.key});
@@ -8,23 +9,33 @@ class AddNoteBottomsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hint: 'title',
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextField(
-              hint: 'content',
-              maxLines: 5,
-            ),
-          ],
+      child: Container(
+        height: 400,
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              SizedBox(
+                height: 32,
+              ),
+              CustomTextField(
+                hint: 'title',
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextField(
+                hint: 'content',
+                maxLines: 5,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomButton(),
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
         ),
       ),
     );
