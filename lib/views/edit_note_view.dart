1adb613a5e2a9f00be3cd65b951fc875458ee43b
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-15 00:27:38 +0200

    create edit view body

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,13 +1,14 @@
 import 'dart:html';
-
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:noteapp/views/widget/edit_view_body.dart';
 
 class EditNoteview extends StatelessWidget {
   const EditNoteview({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: EditNoteview());
+    return Scaffold(body: EditViewBody());
   }
 }
