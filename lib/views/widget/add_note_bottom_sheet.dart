7287fcc5aef276d77de2085566267ad5856c4957
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-15 08:36:38 +0200

    create add note form

--- lib/views/widget/add_note_bottom_sheet.dart
+++ lib/views/widget/add_note_bottom_sheet.dart
@@ -10,31 +10,42 @@ class AddNoteBottomsheet extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hint: 'title',
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextField(
-              hint: 'content',
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 16,
-            ),
-          ],
-        ),
+        child: addNoteForm(),
       ),
+    );
+  }
+}
+
+class addNoteForm extends StatelessWidget {
+  const addNoteForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        SizedBox(
+          height: 32,
+        ),
+        CustomTextField(
+          hint: 'title',
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        CustomTextField(
+          hint: 'content',
+          maxLines: 5,
+        ),
+        SizedBox(
+          height: 32,
+        ),
+        CustomButton(),
+        SizedBox(
+          height: 16,
+        ),
+      ],
     );
   }
 }
