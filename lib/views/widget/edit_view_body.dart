bdb0a6d1c62c86e140fc4f7bf0fa54d3fe3f5ac8
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-15 02:29:07 +0200

    add custom text field to edit viewbody

--- lib/views/widget/edit_view_body.dart
+++ lib/views/widget/edit_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'custom_text_field.dart';
 
 class EditViewBody extends StatelessWidget {
   const EditViewBody({Key? key}) : super(key: key);
@@ -24,6 +25,28 @@ class EditViewBody extends StatelessWidget {
             },
             title: 'Edit Note',
             icon: Icons.check,
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomTextField(
+            onChanged: (value) {
+              title = value;
+            },
+            hint: widget.note.title,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onChanged: (value) {
+              content = value;
+            },
+            hint: widget.note.subTitle,
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 16,
           ),
         ],
       ),
