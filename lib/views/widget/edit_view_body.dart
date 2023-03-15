0e3aebc3c4fe0089416eb632e28b0adc019a85a6
Ayat24 <126516771+Ayat24@users.noreply.github.com>
2023-03-15 02:01:20 +0200

    add padding

--- lib/views/widget/edit_view_body.dart
+++ lib/views/widget/edit_view_body.dart
@@ -7,6 +7,12 @@ class EditViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+     return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
   }
 }
